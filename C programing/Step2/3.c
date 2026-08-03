336dbee360892061606728653cde36ca42346158
PRINCE ARYA <Parya3252@gmail.com>
2026-08-03 16:35:15 +0530

    3.c

--- C programing/Step2/3.c
+++ C programing/Step2/3.c
@@ -5,19 +5,17 @@ int main()
     int count = 0;
     while (no <= 1000)
     {
-        // printf("no");
-        if (no % 2 == 0)
+
+        if (no % 3 &&  no % 5 == 0)
         {
             printf("%d\n", no);
-            // no++;
-           // count++;
+             count++;
         }
         no++;
-      
-        
     }
-   //  printf("%d\n", count);
+     printf("%d\n", count);
 
     return 0;
 }
-// 1 to 1000 with while loop
+
+// 1 to 1000 with while loop where number is divisible by 3 and 5
