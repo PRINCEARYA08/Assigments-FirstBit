7dd4a013cb91a4190301626e70c38fabf4dc24dc
PRINCE ARYA <Parya3252@gmail.com>
2026-08-04 19:45:34 +0530

    3.cpp

---
+++ C++ Practic/3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter the number ";
+    cin>>num;
+    int fact=1;
+    for (int i = 1; i <= num; i++)
+    {
+        fact=fact*i;
+
+    }
+    
+cout<<fact;
+    return 0;
+}
