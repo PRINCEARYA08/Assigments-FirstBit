af45ec2909a4537b30f71221c83befb69e85e72e
PRINCE ARYA <Parya3252@gmail.com>
2026-08-04 19:38:20 +0530

    5.c

--- C programing/Step2/5.c
+++ C programing/Step2/5.c
@@ -1,18 +1,18 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+int main()
+{
 
     int num = 28;
-   int i = 0,sum= 0;
-   while (i<=num/2)
-   {
-if (num%i==0)
-{
-    sum=sum+i;
-
-}
-i++;
-   }
-   printf("%d",sum);
+    int i = 0, sum = 0;
+    while (i <= num / 2)
+    {
+        if (num % i == 0)
+        {
+            sum = sum + i;
+        }
+        i++;
+    }
+    printf("%d", sum);
 
     return 0;
 }
