aa5a502545a83a8f19cbb63e0528cffa9df07ef4
PRINCE ARYA <Parya3252@gmail.com>
2026-09-17 11:58:38 +0530

    sum of two num.

--- Core Python/Demos/addition.py
+++ Core Python/Demos/addition.py
@@ -2,5 +2,11 @@ num1 = int(input('Enter a number 1: '))
 num2 = int(input('Enter a number 2: '))
 
 sum=num1+num2
-print(type(sum))
-print(sum)
+# print(type(sum))
+# print(sum)
+
+# print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+# print('addition of ' +str(num1) + ' and ' + str(num2) + ' is ' + str(sum))
+
+print(f'Addition of {num1} and {num2} is {sum}.')
