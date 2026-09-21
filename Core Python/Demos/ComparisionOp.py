26e76c2a9edce057544dcd55cdc0ccc69de357fe
PRINCE ARYA <Parya3252@gmail.com>
2026-09-21 12:54:51 +0530

    ComparisionOp

---
+++ Core Python/Demos/ComparisionOp.py
@@ -0,0 +1,26 @@
+x = 10
+y = 10
+z = 20
+a = '10'
+#1. == (exactly equal to)
+
+#print(x == y)
+#print(x == a)
+
+#2. !=
+print(x!=a)
+
+#3. >
+print(x>20)
+
+#4. >= (greater than equals to)
+print(z>20)
+print(z>=20)
+
+#5. <
+print(x<20)
+print(x<10)
+print(x<=10)
+
+#6. <= 
+print(z<=20)
