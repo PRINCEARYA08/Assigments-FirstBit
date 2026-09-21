a7188fdfbe332f6141dc1b1a94bf68643d07921e
PRINCE ARYA <Parya3252@gmail.com>
2026-09-21 12:55:05 +0530

    if_demo

---
+++ Core Python/Demos/if_demo.py
@@ -0,0 +1,16 @@
+####Syntax;
+#if(condition):
+    #Block of Code
+    #second
+    #third
+    
+    #if condition is true , block of code will execute 
+    #if condition is false, then block of code will not be execute
+
+
+num = int(input('Enter number: '))
+if(num>0):
+    print('Positive.')
+    
+#else():
+    
