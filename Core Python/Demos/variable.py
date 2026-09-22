470df80c21b994ddb9ef8fe55f3f0a37bbad87c6
PRINCE ARYA <Parya3252@gmail.com>
2026-09-22 19:07:01 +0530

    variable file

--- Core Python/Demos/variable.py
+++ Core Python/Demos/variable.py
@@ -4,8 +4,16 @@
 #int var   #variable declaration
 var = 10  #variable initialization
 
+# in memoery alocation story data in RHS said variable name in LHS
+
+num = 10
+
+print(type(num))  #type of variable checking
+
+
 #2. float
 var = 10.5
 
 print(type(var))  #type of variable checking
 
+# Rules
