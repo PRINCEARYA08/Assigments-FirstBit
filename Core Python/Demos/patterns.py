5262ee75d1e4b07b8dfd054cc8619ed9a246ae4f
PRINCE ARYA <Parya3252@gmail.com>
2026-09-25 18:45:42 +0530

    pattern

---
+++ Core Python/Demos/patterns.py
@@ -0,0 +1,40 @@
+# for i in range(1, 6):
+   
+#    for j in range(1, 8):
+#        print("*", end=" ")
+#    print()
+   
+   
+
+
+
+# for i in range(1, 6):
+   
+#    for j in range(1, i+1):
+#        print("*", end=" ")
+#    print()
+   
+   
+
+
+
+
+
+
+# for i in range(6,0,-1):
+   
+#    for j in range(1, i+1):
+#        print("*", end=" ")
+#    print()
+   
+   
+   
+   
+# range(start, stop, step)
+
+for i in range(6,0,-1):
+   
+   for j in range(i,0,-1):
+       print(j, end=" ")
+   print()
+
